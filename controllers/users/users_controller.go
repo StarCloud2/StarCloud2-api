b851e78e1f4c8ce36bcefa1c2704e325e4dab87c
EvilAcid <achim.grolimund@hf-ict.info>
2021-01-22 22:54:02 +0100

    Update Basic structure and build local with moc database for users

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,33 +1,25 @@
 package users
 
 import (
-	"encoding/json"
 	"github.com/StarCloud2/StarCloud2-api/domain/users"
 	"github.com/StarCloud2/StarCloud2-api/services"
+	"github.com/StarCloud2/StarCloud2-api/utils/errors"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
 
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		//TODO: Handle error
-		log.Fatalln(err)
-		return
-	}
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		//TODO: Handle json error
-		log.Fatalln(err)
-		return
-	}
 	result, err := services.CreateUser(user)
 	if err != nil {
-		//TODO: Handle error
-		log.Fatalln(err)
+		c.JSON(err.Status, err)
 		return
 	}
 
@@ -35,5 +27,18 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("invalid user id. should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
--- services/user_services.go
+++ services/user_services.go
@@ -1,7 +1,28 @@
 package services
 
-import "github.com/StarCloud2/StarCloud2-api/domain/users"
+import (
+	"github.com/StarCloud2/StarCloud2-api/domain/users"
+	"github.com/StarCloud2/StarCloud2-api/utils/errors"
+)
 
-func CreateUser(user users.User) (*users.User, error) {
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+
+}
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
 	return &user, nil
 }
